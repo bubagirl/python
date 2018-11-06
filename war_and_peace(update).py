61562d63d88342ebafd7a9a9f857b44cda07a1ad
Anastasia Turchina <44468971+bubagirl@users.noreply.github.com>
2018-11-06 18:26:02 +0300

    Топ-10 слов

--- war_and_peace(update).py
+++ war_and_peace(update).py
@@ -1,13 +1,17 @@
 from collections import Counter
 import re
-def read_text():
-    Ignore = ['the', 'a','an', 'in', 'at', 'near', 'over', 'under', 'between', 'to', 'from', 'off', 'by'
+
+ignoreList = ['the', 'a','an', 'in', 'at', 'near', 'over', 'under', 'between', 'to', 'from', 'off', 'by'
                   'since', 'for', 'оn', 'in', 'before', 'after', 'and', 'but', 'or', 'if', 'that', 'when', 'of',
                   'so', 'as', 'until', 'because', 'while', 'with']
-    n = "|".join(Ignore)
+n = "|".join(ignoreList)
+c = re.compile(r'\b(?!(?:'+n+'))\w{3,}')
+
+def text_war_and_peace_without_ignoreList():
+
     with open('Tolstoy_War_and_Peace.txt', 'r') as file:
-        text = file.read().lower()
-        words = re.findall(r'\b(?!(?:'+n+'))\w{2,}', text)
+        text = file.read()
+        words = re.findall(c, text)
         top_10 = Counter(words).most_common(10)
         print(top_10)
-read_text()
+text_war_and_peace_without_ignoreList()
